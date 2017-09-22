09814147bd98ffd52d0ab618c2a5559796317cba
laraismael1 <laraismael123@gmail.com>
2017-09-22 12:31:28 -0700

    created shema for db

--- database/index.js
+++ database/index.js
@@ -1,16 +1,37 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+var db = mongoose.connection;
+
+db.on('error', function() {
+  console.log('mongoose connection error');
+});
+
+db.once('open', function() {
+  console.log('mongoose connected successfully');
+});
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  name: String,
+  repos: String,
+  watchers: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (callback) => {
+	
+	var newRepo = new Repo ({
+		name: name,
+		repos: owner.repos_url,
+		watchers: watchers
+	})
+
+	newRepo.save( (err, newRepo) => {
+		if (err) { console.log(err); }
+		callback(newRepo);
+	})
+		
 }
 
 module.exports.save = save;
